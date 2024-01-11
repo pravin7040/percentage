309ef627779421ca14477a5a144971c1dea63962
Pravin  Padir <156301718+pravin7040@users.noreply.github.com>
2024-01-11 14:54:28 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,31 @@
+# 5). Python program to get the Average of given numbers.
+# Formula: sum of all the number/ total number
+# Input:
+# a = 40
+# b = 50
+# c = 30
+# Output :
+# Average = 40
+
+a=40
+b=50
+c=30
+print((a+b+c)/3)
+
+
+# 6). Python program to get the median of given numbers.
+# Note: all the numbers should be arranged in ascending order
+# Formula : (n+1)/2
+# n = Number of values
+# Input : [45, 60, 61, 66, 70, 77, 80]
+# Output:  66
+list1 = [45, 60, 61, 66, 70, 77, 80]
+list1.sort()
+a = (len(list1))/2
+print("Median: ",list1[int(a)])
+
+
+
+
+
+
